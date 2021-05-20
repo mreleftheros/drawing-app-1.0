7327b372a533d0a72463bd7dbaf6cf1f34d66b6c
eleftheros <eleftheros_all@protonmail.com>
2021-05-20 11:38:33 +0300

    add checkEventType function

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -5,23 +5,25 @@ const ctx = canvas.getContext("2d");
 let radius = 10;
 let isClicked = false;
 
+//function to check event type and set isClicked value
+const checkEventType = e => {
+  if(e.type === "mousedown")
+    isClicked = true;
+  else if(e.type === "mouseup")
+    isClicked = false;
+};
+
+//function to draw
 const putPoint = e => {
-  
+  checkEventType(e);
+
   if(isClicked) {
     ctx.beginPath();
     ctx.arc(e.x, e.y, radius, 0, Math.PI * 2);
     ctx.fill();
   }
-  
 };
 
-document.addEventListener("mousedown", e => {
-  console.log(e)
-  isClicked = true;
-  putPoint(e);
-});
-document.addEventListener("mouseup", e => isClicked = false);
-document.addEventListener("mousemove", e => {
-  if(isClicked)
-    putPoint(e);
-})
+document.addEventListener("mousedown", putPoint);
+document.addEventListener("mouseup", putPoint);
+document.addEventListener("mousemove", putPoint);
