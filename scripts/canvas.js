9d1f033b28614855d48855f4387bd98f5bcf4cbd
eleftheros <eleftheros_all@protonmail.com>
2021-05-20 11:24:23 +0300

    Add mouse functions

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,3 +1,27 @@
 const canvas = document.getElementById("canvas");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+const ctx = canvas.getContext("2d");
+let radius = 10;
+let isClicked = false;
+
+const putPoint = e => {
+  
+  if(isClicked) {
+    ctx.beginPath();
+    ctx.arc(e.x, e.y, radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
+  
+};
+
+document.addEventListener("mousedown", e => {
+  console.log(e)
+  isClicked = true;
+  putPoint(e);
+});
+document.addEventListener("mouseup", e => isClicked = false);
+document.addEventListener("mousemove", e => {
+  if(isClicked)
+    putPoint(e);
+})
