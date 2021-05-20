e0bea1bd239ffc21ba52580113a764c9e06bb7f2
eleftheros <eleftheros_all@protonmail.com>
2021-05-20 12:05:53 +0300

    make smooth lines

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -3,6 +3,7 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 const ctx = canvas.getContext("2d");
 let radius = 10;
+ctx.lineWidth = 2 * radius;
 let isClicked = false;
 
 //function to check event type and set isClicked value
@@ -18,9 +19,15 @@ const putPoint = e => {
   checkEventType(e);
 
   if(isClicked) {
+    ctx.lineTo(e.x, e.y);
+    ctx.stroke();
     ctx.beginPath();
     ctx.arc(e.x, e.y, radius, 0, Math.PI * 2);
     ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(e.x, e.y);
+  } else {
+    ctx.beginPath();
   }
 };
 
